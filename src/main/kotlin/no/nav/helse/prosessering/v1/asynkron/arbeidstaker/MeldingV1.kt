fe7360b32dcd263e3dbc413ce74a7f85b4dcea02
SanderOpperud <60642441+SanderOpperud@users.noreply.github.com>
2021-04-09 10:21:35 +0200

    Renamer fosterbarn.fødselsnummer til identitetsnummer. (#47)
    Oppdaterer tester.

--- src/main/kotlin/no/nav/helse/prosessering/v1/asynkron/arbeidstaker/MeldingV1.kt
+++ src/main/kotlin/no/nav/helse/prosessering/v1/asynkron/arbeidstaker/MeldingV1.kt
@@ -1,5 +1,6 @@
 package no.nav.omsorgspengerutbetaling.arbeidstakerutbetaling
 
+import com.fasterxml.jackson.annotation.JsonAlias
 import com.fasterxml.jackson.annotation.JsonCreator
 import com.fasterxml.jackson.annotation.JsonFormat
 import com.fasterxml.jackson.annotation.JsonValue
@@ -95,7 +96,8 @@ data class Søker(
 }
 
 data class FosterBarn(
-    val fødselsnummer: String
+    @JsonAlias("fødselsnummer")
+    val identitetsnummer: String
 ) {
     override fun toString(): String {
         return "FosterBarn()"

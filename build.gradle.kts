91508e64bf46494e9acb1f6dd4260239a36b516b
albrektsson <emil.albrektsson@nav.no>
2022-09-13 08:32:36 +0200

    Fjernet ingresses, ktor + gradle bump

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 val mainClass = "no.nav.helse.OmsorgspengerutbetalingeSoknadProsesseringKt"
-val dusseldorfKtorVersion = "3.2.1.0-f35fe61"
+val dusseldorfKtorVersion = "3.2.1.1-15e2e63"
 val ktorVersion = ext.get("ktorVersion").toString()
 
 val k9FormatVersion = "6.1.5"
@@ -103,7 +103,7 @@ tasks.withType<ShadowJar> {
 }
 
 tasks.withType<Wrapper> {
-    gradleVersion = "7.4.2"
+    gradleVersion = "7.5.1"
 }
 
 tasks.withType<Test> {
